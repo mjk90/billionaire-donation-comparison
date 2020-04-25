26c709b59c16cf59aa65d6e51b4beb076598f026
Matthew Kelly <matthew.kelly@block.one>
2020-04-25 15:42:26 +0800

    Updates

---
+++ precache-manifest.389a4f7ed816c15fcd55d65d2d45b9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d44380940db3d46607eb839f23ddde",
+    "url": "/billionaire-donation-comparison/index.html"
+  },
+  {
+    "revision": "9a7ae8c78acb0c551892",
+    "url": "/billionaire-donation-comparison/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7ba51c7dcc334a7af9c3",
+    "url": "/billionaire-donation-comparison/static/css/main.b7bbcf47.chunk.css"
+  },
+  {
+    "revision": "9a7ae8c78acb0c551892",
+    "url": "/billionaire-donation-comparison/static/js/2.942af859.chunk.js"
+  },
+  {
+    "revision": "9c73234f85fa5d47d2045b41bc69eb9f",
+    "url": "/billionaire-donation-comparison/static/js/2.942af859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba51c7dcc334a7af9c3",
+    "url": "/billionaire-donation-comparison/static/js/main.7bd1d2e4.chunk.js"
+  },
+  {
+    "revision": "49d441b55d4727491fb3",
+    "url": "/billionaire-donation-comparison/static/js/runtime-main.d6b5a1ae.js"
+  }
+]);
